b789ab81ec5ff91fd497f1c73ebe813d128900d0
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-11-05 16:36:52 -0300

    Create teste_cadastrar_ator.py

---
+++ BackEnd/tests/teste_cadastrar_ator.py
@@ -0,0 +1,12 @@
+# Teste do cadastro de atores (cadastro real no banco)
+
+from BackEnd.api.endpoints.atores import cadastrar_ator
+
+def test():
+  nome = input("Nome do ator: ")
+  foto = input("URL da foto: ")
+  
+  cadastrar_ator(nome, foto)
+
+if __name__ == "__main__":
+    test()
