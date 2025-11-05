52f9522feb0c3c7ace48640daf3a9892266f34eb
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-11-05 14:44:34 -0300

    Delete BackEnd/setup_path.py

--- BackEnd/setup_path.py
+++
@@ -1,8 +0,0 @@
-# Scripts não localizam a pasta BackEnd -> arrumando o bug
-
-import sys, os
-
-# adiciona a pasta BackEnd ao sys.path
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if ROOT_DIR not in sys.path:
-    sys.path.append(ROOT_DIR)
